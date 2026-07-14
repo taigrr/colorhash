826e80a3cdd331b76bf4e41fc1452e418ea74c67
Tai Groot <tai@taigrr.com>
2026-07-14 08:04:53 +0000

    feat(oklch): support hue-offset palette generation

--- oklch.go
+++ oklch.go
@@ -3,6 +3,8 @@
 package colorhash
 
 import (
+	"math"
+
 	"github.com/taigrr/simplecolorpalettes/simplecolor"
 )
 
@@ -10,14 +12,28 @@ import (
 // at the given lightness and chroma. This produces a perceptually uniform palette
 // where all colors appear equally bright and saturated.
 func GenerateOKLCHPalette(n int, l, c float64) simplecolor.SimplePalette {
+	return GenerateOKLCHPaletteWithHueOffset(n, l, c, 0)
+}
+
+// GenerateOKLCHPaletteWithHueOffset generates n evenly-spaced colors in the
+// OKLCH color space, starting at startHue degrees.
+func GenerateOKLCHPaletteWithHueOffset(n int, l, c, startHue float64) simplecolor.SimplePalette {
 	if n <= 0 {
 		return simplecolor.SimplePalette{}
 	}
 	palette := make(simplecolor.SimplePalette, n)
 	step := 360.0 / float64(n)
 	for i := 0; i < n; i++ {
-		h := float64(i) * step
+		h := normalizeHue(startHue + float64(i)*step)
 		palette[i] = simplecolor.FromOKLCH(l, c, h)
 	}
 	return palette
+}
+
+func normalizeHue(h float64) float64 {
+	h = math.Mod(h, 360)
+	if h < 0 {
+		h += 360
+	}
+	return h
 }
